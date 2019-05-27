28475844160961e0436ea9e7dc05b5948c018a8c
RaviReddyFinsol <gajareddyravi@gmail.com>
2019-05-27 20:22:29 +0530

    ed

--- src/components/catogery/group/ViewAllGroups.js
+++ src/components/catogery/group/ViewAllGroups.js
@@ -18,7 +18,7 @@ componentDidMount(){
     render(){
         return(
             <div>
-                {this.state.groups.length !== 0 ? (this.state.groups.map(group => <ViewGroup key={group.id} imagePath={group.imagePath} groupName={group.groupName}/>)) : (<h2> {this.state.notification}</h2>)}
+                {this.state.groups.length !== 0 ? (this.state.groups.map(group => <ViewGroup key={group.id} imagePath={group.imagePath} groupName={group.groupName} isEditable={group.isEditable}/>)) : (<h2> {this.state.notification}</h2>)}
             </div>
         )
     }
--- src/components/catogery/group/ViewGroup.js
+++ src/components/catogery/group/ViewGroup.js
@@ -1,4 +1,4 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -9,32 +9,36 @@ import Typography from '@material-ui/core/Typography';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 
-class ViewGroup extends Component
-{
+class ViewGroup extends Component {
 
-    render(){
-        return(
-            <Card>
-            <CardActionArea>
-              <CardMedia
-                image="https://www.pexels.com/photo/scenic-view-of-beach-248797/" />
-              <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {this.props.groupName}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-               <EditIcon />
-              </Button>
-              <Button size="small" color="primary">
-              <DeleteIcon />
-              </Button>
-            </CardActions>
-          </Card>
-        )
-    }
+  render() {
+    return (
+      <Card>
+        <CardActionArea>
+          <CardMedia
+            image="https://www.pexels.com/photo/scenic-view-of-beach-248797/" />
+          <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {this.props.groupName}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        {
+          this.props.isEditable ?
+            (
+              <CardActions>
+                <Button size="small" color="primary">
+                  <EditIcon />
+                </Button>
+                <Button size="small" color="primary">
+                  <DeleteIcon />
+                </Button>
+              </CardActions>
+            ) : ""}
+
+      </Card>
+    )
+  }
 }
 
 export default ViewGroup;
